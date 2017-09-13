29ebfe83fb384deff68f947ab70504124984d28f
sscaglione <sscaglio@nd.edu>
2017-09-12 20:58:47 -0400

    add recycle 2 screen

---
+++ app/src/main/java/com/example/proyectotiti/recycle2.java
@@ -0,0 +1,13 @@
+package com.example.proyectotiti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class recycle2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle2);
+    }
+}
