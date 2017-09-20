6330bd9314df9b72d841b0b5478695ba39f8867c
katieschermerhorn <kscherme@nd.edu>
2017-09-19 22:42:05 -0400

    Added firebase to project

--- app/src/main/java/com/example/proyectotiti/MainActivity.java
+++ app/src/main/java/com/example/proyectotiti/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.proyectotiti;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
