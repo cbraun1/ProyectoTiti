ed8aec6dad38c8f67a08efe10da95a7eb0921938
Katie Schermerhorn <katieschermerhorn@katies-air.dhcp.nd.edu>
2017-09-25 17:16:59 -0400

    Implemented simple onclick function for login

--- app/src/main/java/com/example/proyectotiti/MainActivity.java
+++ app/src/main/java/com/example/proyectotiti/MainActivity.java
@@ -3,16 +3,41 @@ package com.example.proyectotiti;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText userInput, passInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.startButton);
+        userInput = (EditText)findViewById(R.id.usernameInput);
+        passInput = (EditText)findViewById(R.id.passwordInput);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(userInput.getText().toString().equals("admin") &&
+                        passInput.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Redirecting...",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
 
     }
 }
