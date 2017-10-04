62b256bdb87a12280fc91aa3c90d93b3f38644da
sscaglione <sscaglio@nd.edu>
2017-10-04 11:48:28 -0400

    working on layouts

--- app/src/main/java/com/example/proyectotiti/continuePage.java
+++ app/src/main/java/com/example/proyectotiti/continuePage.java
@@ -11,3 +11,42 @@ public class continuePage extends AppCompatActivity {
         setContentView(R.layout.activity_continue_page);
     }
 }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkbox_family1:
+                if (checked)
+                // download
+            else
+                // don't download
+                break;
+            case R.id.checkbox_family2:
+                if (checked)
+                // download
+            else
+                // don't download
+                break;
+            case R.id.checkbox_family3:
+                if (checked)
+                // download
+            else
+                // don't download
+                break;
+            case R.id.checkbox_family4:
+                if (checked)
+                // download
+            else
+                // don't download
+                break;
+            case R.id.checkbox_family5:
+                if (checked)
+                // download
+            else
+                // don't download
+                break;
+        }
+    }
