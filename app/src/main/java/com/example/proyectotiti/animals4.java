53445b369940499ffd843eef96afe40cce493342
sscaglione <sscaglio@nd.edu>
2017-10-22 20:03:09 -0400

    working on layouts

--- app/src/main/java/com/example/proyectotiti/animals4.java
+++ app/src/main/java/com/example/proyectotiti/animals4.java
@@ -3,11 +3,11 @@ package com.example.proyectotiti;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class addFamilies extends AppCompatActivity {
+public class animals4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_families);
+        setContentView(R.layout.activity_animals4);
     }
 }
