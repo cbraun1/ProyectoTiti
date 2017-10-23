185c621bc6838da7c7fac4fe1f99a5634650d845
sscaglione <sscaglio@nd.edu>
2017-10-23 13:30:14 -0400

    added button functionality to basicData

--- app/src/main/java/com/example/proyectotiti/basicData.java
+++ app/src/main/java/com/example/proyectotiti/basicData.java
@@ -1,7 +1,9 @@
 package com.example.proyectotiti;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class basicData extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class basicData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_data);
+    }
+
+    public void openContinue(View v){
+        startActivity(new Intent(basicData.this, continuePage.class));
+    }
+
+    public void openAnimals0(View v){
+        startActivity(new Intent(basicData.this, animals0.class));
     }
 }
