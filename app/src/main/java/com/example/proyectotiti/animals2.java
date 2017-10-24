5c45d20d537aa7644d2d093a891c37fd582f982d
sscaglione <sscaglio@nd.edu>
2017-10-24 11:58:48 -0400

    added button functionality to animals2

--- app/src/main/java/com/example/proyectotiti/animals2.java
+++ app/src/main/java/com/example/proyectotiti/animals2.java
@@ -1,7 +1,9 @@
 package com.example.proyectotiti;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class animals2 extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class animals2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animals2);
+    }
+
+    public void openAnimals1(View v){
+        startActivity(new Intent(animals2.this, animals1.class));
+    }
+
+    public void openAnimals3(View v){
+        startActivity(new Intent(animals2.this, animals3.class));
     }
 }
