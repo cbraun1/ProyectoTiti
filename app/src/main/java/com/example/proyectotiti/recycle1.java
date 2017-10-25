e8055f8b37b4a72c089d7ee14bb0b14a1d437925
sscaglione <sscaglio@nd.edu>
2017-10-25 08:52:15 -0400

    debugging and adding button and radio button functionality to recycle1

--- app/src/main/java/com/example/proyectotiti/recycle1.java
+++ app/src/main/java/com/example/proyectotiti/recycle1.java
@@ -1,7 +1,11 @@
 package com.example.proyectotiti;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class recycle1 extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class recycle1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle1);
+
+        final RadioButton radioButtonSi = (RadioButton) findViewById(R.id.radioButtonSi);
+        final RadioButton radioButtonNo = (RadioButton) findViewById(R.id.radioButtonNo);
+
+        final Button continueButton = (Button) findViewById(R.id.continueButton);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                if(radioButtonSi.isChecked()){
+                    startActivity(new Intent(recycle1.this, recycle2.class));
+                }
+                else
+                {
+                    if(radioButtonNo.isChecked())
+                    {
+                        startActivity(new Intent(recycle1.this, recycle3.class));
+                    }
+                }
+            }
+        });
     }
 }
