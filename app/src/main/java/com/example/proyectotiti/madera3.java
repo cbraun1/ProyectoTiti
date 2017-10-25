b8a1ab24e068d23bcd6f1b71b674edc78124b375
sscaglione <sscaglio@nd.edu>
2017-10-25 08:29:55 -0400

    adding button functionality to madera3

--- app/src/main/java/com/example/proyectotiti/madera3.java
+++ app/src/main/java/com/example/proyectotiti/madera3.java
@@ -1,7 +1,9 @@
 package com.example.proyectotiti;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class madera3 extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class madera3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_madera3);
+    }
+    public void openMadera0(View v){
+        startActivity(new Intent(madera3.this, madera0.class));
+    }
+    public void openMadera4(View v){
+        startActivity(new Intent(madera3.this, madera4.class));
     }
 }
