c37d037612d147395a185fa68056834a633f1791
sscaglione <sscaglio@nd.edu>
2017-10-25 08:37:35 -0400

    debugging and adding button functionality to madera4

--- app/src/main/java/com/example/proyectotiti/madera4.java
+++ app/src/main/java/com/example/proyectotiti/madera4.java
@@ -1,7 +1,11 @@
 package com.example.proyectotiti;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class madera4 extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class madera4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_madera4);
+
+        final RadioButton radioButtonSi = (RadioButton) findViewById(R.id.radioButtonSi);
+        final RadioButton radioButtonNo = (RadioButton) findViewById(R.id.radioButtonNo);
+
+        final Button continueButton = (Button) findViewById(R.id.continueButton);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                if(radioButtonSi.isChecked()){
+                    startActivity(new Intent(madera4.this, madera5.class));
+                }
+                else
+                {
+                    if(radioButtonNo.isChecked())
+                    {
+                        startActivity(new Intent(madera4.this, recycle1.class));
+                    }
+                }
+            }
+        });
     }
 }
