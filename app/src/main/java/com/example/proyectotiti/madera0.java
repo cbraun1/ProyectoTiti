6f751ff4759a61b1b47cdb55dd23b20fc3cf80c1
sscaglione <sscaglio@nd.edu>
2017-10-24 20:02:21 -0400

    adding button functionality animals 4

--- app/src/main/java/com/example/proyectotiti/madera0.java
+++ app/src/main/java/com/example/proyectotiti/madera0.java
@@ -1,7 +1,9 @@
 package com.example.proyectotiti;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class madera0 extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class madera0 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_madera0);
+    }
+
+    public void openMadera1(View v){
+        startActivity(new Intent(madera0.this, madera1.class));
+    }
+
+    public void openMadera2(View v){
+        startActivity(new Intent(madera0.this, madera2.class));
+    }
+    public void openMadera3(View v){
+        startActivity(new Intent(madera0.this, madera3.class));
+    }
+    public void openAnimals0(View v){
+        startActivity(new Intent(madera0.this, animals0.class));
     }
 }
